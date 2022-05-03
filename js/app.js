91941006b15180a2a9791cbae40357e6885a3b8c
Zac Stanley <56239508+zac-stanley@users.noreply.github.com>
2022-05-02 17:08:00 -0700

    update index.html, app.js, and stlye.css

---
+++ js/app.js
@@ -0,0 +1,20 @@
+(function(){
+// initial Leaflet map options
+const options = {
+    zoomSnap: .1,
+    // center: [40, -90], 
+    // zoom: 4,
+    zoomControl: false
+}
+// create Leaflet map and apply options
+const map = L.map('map', options);
+new L.control.zoom({ position: "bottomright" }).addTo(map)
+
+// create Leaflet map and apply options
+//	const map = L.map('map'); //options);
+
+// request a basemap tile layer and add to the map
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+})();
