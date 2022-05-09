772f05e6219851d126bbad9ec1c91dc025ff2a3a
Zac Stanley <56239508+zac-stanley@users.noreply.github.com>
2022-05-09 16:38:26 -0700

    update json's, index.html and app.js

--- js/app.js
+++ js/app.js
@@ -1,168 +1,159 @@
-(function(){
-// initial Leaflet map options
-const options = {
-    zoomSnap: .1,
-    center: [39, -98.58], 
-    zoom: 5.4,
-    zoomControl: false
-}
-// create Leaflet map and apply options
-const map = L.map('map', options);
-new L.control.zoom({ position: "bottomright" }).addTo(map)
+(function () {
+    // initial Leaflet map options
+    const options = {
+        zoomSnap: .1,
+        center: [39, -98.58],
+        zoom: 5.3,
+        zoomControl: false
+    }
+    // create Leaflet map and apply options
+    const map = L.map('map', options);
+    new L.control.zoom({ position: "bottomright" }).addTo(map)
 
-// request a basemap tile layer and add to the map
-// L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-L.tileLayer('https://api.mapbox.com/styles/v1/zacstanley/cl26t5a9k001e15o319at3jeh/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiemFjc3RhbmxleSIsImEiOiJCS20zaVR3In0._oaGhAVLz04gbE3M2HKHGA', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    // request a basemap tile layer and add to the map
+    // L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/zacstanley/cl26t5a9k001e15o319at3jeh/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiemFjc3RhbmxleSIsImEiOiJCS20zaVR3In0._oaGhAVLz04gbE3M2HKHGA', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
 
 
-// create styles common to all points
-const commonStyles = {
-    weight: 1,
-    stroke: 1,
-    fillOpacity: .8
-}
-
-// jQuery method using AJAX request for GeoJSON data
-// add sviPoint data
-$.getJSON("data/cdps_svis_whp_ctr.json", function (sviPoints) {
-    console.log(sviPoints)
-    drawMap(sviPoints);
-
-    function drawMap (sviPoints){
-        var options = {
-
-            pointToLayer: pointToLayer,
-            style: style,
-            onEachFeature: onEachFeature
-        }
-        L.geoJson(sviPoints, options).addTo(map);
+    // create styles common to all points
+    const commonStyles = {
+        weight: 1,
+        stroke: 1,
+        fillOpacity: .8
     }
 
-    function pointToLayer(feature, latlng) {
-        // function will take Point Feature geometry
-        // and convert to a Leaflet layer by returning
-        // a Leaflet marker or SVG such as circle or circleMarker
+    // jQuery method using AJAX request for GeoJSON point data
+    // add sviPoint data
+    $.getJSON("data/cdps_svis_whp_ctr.json", function (sviPoints) {
+        console.log(sviPoints)
+        drawMap(sviPoints);
 
-        return L.circleMarker(latlng, {
+        function drawMap(sviPoints) {
+            var options = {
 
-            radius: calcRadius(feature.properties.OVERALL_WM)
-
-        });
-    }
-
-    function style(feature) {
-
-        var styleOptions = {
-
-            fillOpacity: .6,
-            color: "whitesmoke",
-            weight: .1
-        }
-
-        if (feature.properties.WHP_CLASS === 'Very High') {
-            styleOptions.fillColor = '#ffc937';
-        }
-
-        if (feature.properties.WHP_CLASS === 'Moderate') {
-            styleOptions.fillColor = '#384051';
-        } 
-
-        if (feature.properties.WHP_CLASS === 'High') {
-            styleOptions.fillColor = '#f05449';
-        }
-
-        return styleOptions;
-    }
-
-    function calcRadius(val) {
-
-        var radius = Math.sqrt(val / Math.PI);
-        return radius * 25;
-
-    }
-
-    function onEachFeature(feature, layer) {
-
-        layer.on({
-            mouseover: function () {
-
-                layer.setStyle({
-                    color: "yellow",
-                    weight: 3
-                });
-
-            },
-            mouseout: function () {
-
-                layer.setStyle({
-                    fillOpacity: .6,
-                    color: "whitesmoke",
-                    weight: .1
-                });
+                pointToLayer: pointToLayer,
+                style: style,
+                onEachFeature: onEachFeature
             }
-        });
+            L.geoJson(sviPoints, options).addTo(map);
+        }
 
-               var popupInfo = `<h3>${feature.properties.CDP_STATE}</h3>
+        function pointToLayer(feature, latlng) {
+            // function will take Point Feature geometry
+            // and convert to a Leaflet layer by returning
+            // a Leaflet marker or SVG such as circle or circleMarker
+
+            return L.circleMarker(latlng, {
+
+                radius: calcRadius(feature.properties.OVERALL_WM)
+
+            });
+        }
+
+        function style(feature) {
+
+            var styleOptions = {
+
+                fillOpacity: .6,
+                color: "whitesmoke",
+                weight: .1
+            }
+
+            if (feature.properties.WHP_CLASS === 'Very High') {
+                styleOptions.fillColor = '#ffc937';
+            }
+
+            if (feature.properties.WHP_CLASS === 'Moderate') {
+                styleOptions.fillColor = '#384051';
+            }
+
+            if (feature.properties.WHP_CLASS === 'High') {
+                styleOptions.fillColor = '#f05449';
+            }
+
+            return styleOptions;
+        }
+
+        function calcRadius(val) {
+
+            var radius = Math.sqrt(val / Math.PI);
+            return radius * 25;
+
+        }
+
+        function onEachFeature(feature, layer) {
+
+            layer.on({
+                mouseover: function () {
+
+                    layer.setStyle({
+                        color: "yellow",
+                        weight: 3
+                    });
+
+                },
+                mouseout: function () {
+
+                    layer.setStyle({
+                        fillOpacity: .6,
+                        color: "whitesmoke",
+                        weight: .1
+                    });
+                }
+            });
+
+            var popupInfo = `<h3>${feature.properties.CDP_STATE}</h3>
         <h4>Overall SVI Score: <b>${feature.properties.OVERALL_WM}</b></h4>
         <h4>Wildfire Hazard Potential: <b>${feature.properties.WHP_CLASS}</b></h4>`
 
 
-        layer.bindPopup(popupInfo);
+            layer.bindPopup(popupInfo);
 
-    }
+        }
 
-    // var moderateLayer = L.geoJson(sviPoints, {
-    //     pointToLayer: function (feature, latlng) {
-    //         // Draw circle marker based on pointToLayer function
-    //         return L.circleMarker(latlng, commonStyles);
-    //     },
-//         // filter layer to display only class value of moderate
-// 					filter: function (feature) {
-// 						if (feature.properties.WHP_CLASS.Moderate) {
-// 							return feature;
-// 						}
-// 					},
-//                     // style stroke and fill
-// 					style: function (feature) {
-// 						return {
-// 							color: '#FF4000',
-// 							fillColor: '#FF4000',
-// 							// call function get radius from overall svi weighted mean
-// 							radius: getRadius(feature.properties.OVERALL_WM)
-// 						}
-//                     },
-// 					onEachFeature: function (feature, layer) {
-// 						// on mouseover change color to darker shade of orange
-// 						layer.on('mouseover', function () {
-// 							layer.setStyle({
-// 								fillColor: '#401000'
-// 							});
-// 						});
-// 						// return to original fill color when not hovering with mouse
-// 						layer.on('mouseout', function () {
-// 							layer.setStyle({
-// 								fillColor: '#FF4000'
-// 							});
-// 						});
+    });
 
-// 						var popup = `<h4>${feature.properties.CDP_STATE}</h4>
-//                         <p><b>Overall SVI:</b> ${feature.properties.OVERALL_WM}<br>
-//                         <p><b>Wildfire Hazard Potential:</b> ${feature.properties.WHP_CLASS}<br>`
+    // jQuery method using AJAX request for GeoJSON polygon data
+    // add sviPolys data
+    $.getJSON("data/cdps_svis_whp.json", function (sviPolys) {
+        console.log(sviPolys)
+        drawMap(sviPolys);
 
-// 						// Loop through all fuel sources in plants that split atoms
-// 						// for (var fuelSource in feature.properties.fuel_source) {
-// 						// 	// View the source
-// 						// 	console.log(fuelSource)
-// 						// 	//  Concat all fuel sources 
-// 						// 	popup += `<br>${fuelSource}: ${feature.properties.fuel_source[fuelSource]} MW`
-// 						// }
+        function drawMap(sviPolys) {
+            var options = {
 
-// 						layer.bindPopup(popup)
-// 					}
-				// }).addTo(map);
+                // pointToLayer: pointToLayer,
+                 style: style,
+                // onEachFeature: onEachFeature
+            }
+            L.geoJson(sviPolys, options).addTo(map);
+        }
 
-});
+        function style(feature) {
+
+            var styleOptions = {
+
+                fillOpacity: .6,
+                color: "whitesmoke",
+                weight: .1
+            }
+
+            if (feature.properties.WHP_CLASS === 'Very High') {
+                styleOptions.fillColor = '#ffc937';
+            }
+
+            if (feature.properties.WHP_CLASS === 'Moderate') {
+                styleOptions.fillColor = '#384051';
+            }
+
+            if (feature.properties.WHP_CLASS === 'High') {
+                styleOptions.fillColor = '#f05449';
+            }
+
+            return styleOptions;
+        }
+    });
 })();
--- other/ToDos.md
+++ other/ToDos.md
@@ -12,18 +12,18 @@
 #### HTML/CSS/LAYOUT
 1. Clean up hyperlink code for svg
 2. Push info svg further to right so it's tight against search box
-3. What is the purple area in row class on right of screen
-4. Add mobile CSS including map centering
+3. ~~What is the purple area in row class on right of screen~~
+4. Put info SVG and search bar in same div so they always stay in the place.
 
 
 #### Basemap
 1. Create basemap with mapbox
 
 #### JS
-1. Plot graduated symbols
+1. ~~Plot graduated symbols~~
 2. Plot polygons with color symbology
 3. Create static legend for colors and graduated symbols - https://roh.engineering/posts/2021/05/map-symbols-and-size-legends-for-leaflet/
 4. Make search bar usable based on CDP input, might need to concatenate fields in Pandas to include CDP and State together
-
+5. ~~Create popups and format them for graduated symbols~~
 
 
