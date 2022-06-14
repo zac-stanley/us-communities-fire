d0899a09b53c9ef4e82ca056fb0dc27fd7109cd6
Zac Stanley <56239508+zac-stanley@users.noreply.github.com>
2022-06-14 16:55:00 -0700

    Update full scale basemap

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
     const options = {
         zoomSnap: .1,
         center: [39, -98.58],
-        zoom: 5.3,
+        zoom: 3.77,
         zoomControl: false
     }
     // create Leaflet map and apply options
@@ -15,10 +15,12 @@
     new L.control.zoom({ position: "topleft" }).addTo(map)
 
     // request a basemap tile layer and add to the map
-    var lightBase = L.tileLayer('https://api.mapbox.com/styles/v1/zacstanley/cl26t5a9k001e15o319at3jeh/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiemFjc3RhbmxleSIsImEiOiJCS20zaVR3In0._oaGhAVLz04gbE3M2HKHGA', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    var lightBase = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         subdomains: 'abcd',
-        maxZoom: 19
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
     });
 
     // request an imagery basemap tile layer and add to the map
